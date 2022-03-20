fbc9691175bd439346136d46c4c8383b460e3e7c
matejsa <matejsarlija1@gmail.com>
2022-03-20 17:29:29 +0100

    printing stuff out

--- src/main/java/com/matej/learn/App.java
+++ src/main/java/com/matej/learn/App.java
@@ -111,6 +111,21 @@ public final class App {
 
         aLongName.ifPresent(name -> System.out.println(String.format("A longest name: %s", name)));
 
+        for(String name: friends) {
+            System.out.print(name + ", ");
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < friends.size() - 1; i++) {
+            System.out.print(friends.get(i) + ", ");
+        }
+
+        if (friends.size() > 0)
+            System.out.println(friends.get(friends.size() - 1));
+
+        System.out.println(String.join(", ", friends));
+
         
     }
 }
