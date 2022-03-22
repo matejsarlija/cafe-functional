99b0d6ff2a4bb996952b9d66099d0db0d1a8884d
matejsa <matejsarlija1@gmail.com>
2022-03-22 19:14:14 +0100

    person aka people

--- src/main/java/com/matej/learn/App.java
+++ src/main/java/com/matej/learn/App.java
@@ -2,6 +2,7 @@ package com.matej.learn;
 
 import java.util.*;
 import java.util.function.*;
+import java.util.stream.Collectors;
 
 /**
  * Hello world!
@@ -142,6 +143,22 @@ public final class App {
         .mapToObj(ch -> Character.valueOf((char) ch))
         .forEach(System.out::println);
 
+        // people
+
+
+        final List<Person> people = Arrays.asList(
+            new Person("John", 20),
+            new Person("Sara", 21),
+            new Person("Amy", 21),
+            new Person("Jane", 21),
+            new Person("Greg", 35)
+        );
+
+        List<Person> ascendingAge =
+        people.stream().sorted((person1, person2) -> person1.ageDifference(person2))
+        .collect(Collectors.toList());
+
+        printPeople("Sorted in ascending order by age: ", ascendingAge);
         
     }
 }
