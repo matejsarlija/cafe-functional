06156aa292eb9bad81470d9dbf56cc410e8507f0
matejsa <matejsarlija1@gmail.com>
2022-03-30 20:49:00 +0200

    exceptions

--- src/main/java/com/matej/learn/Series.java
+++ src/main/java/com/matej/learn/Series.java
@@ -1,7 +1,17 @@
 package com.matej.learn;
 
 public interface Series {
-    int getNext();
+    int getNext(); // return next number in series
+
+    // Returns an array that contains the next n elements
+    // in the series beyond the current element.
+    default int[] getNextArray(int n) {
+        int[] vals = new int[n];
+
+        for(int i= 0; i < n; i++) vals[i] = getNext();
+        return vals;
+    }
+
     void reset();
     void setStart(int x);
 }
