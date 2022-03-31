b31db5416d83a282d38d7902524548b43e49f814
matejsa <matejsarlija1@gmail.com>
2022-03-31 19:57:30 +0200

    io refactor

--- src/main/java/com/matej/learn/CopyFile.java
+++ src/main/java/com/matej/learn/CopyFile.java
@@ -5,8 +5,6 @@ import java.io.*;
 public class CopyFile {
     public static void main(String[] args) throws IOException {
         int i;
-        FileInputStream fin = null;
-        FileOutputStream fout = null;
 
         // first make sure that both files have been specified
         if(args.length != 2) {
@@ -15,28 +13,15 @@ public class CopyFile {
         }
 
         // Copy a file
-        try {
-            // Attempt to open the files
-            fin = new FileInputStream(args[0]);
-            fout = new FileOutputStream(args[1]);
-            
+        try (FileInputStream fin = new FileInputStream(args[0]);
+            FileOutputStream fout = new FileOutputStream(args[1])) {
             do {
                 i = fin.read();
                 if (i != -1) fout.write(i);
             } while (i != -1);
+
         } catch (IOException exc) {
             System.out.println("I/O Error: " + exc);
-        } finally {
-            try {
-                if (fin != null) fin.close();
-            } catch (IOException exc) {
-                System.out.println("Error closing input file.");
-            } 
-            try {
-                if (fout != null) fout.close();
-            } catch (IOException exc) {
-                System.out.println("Error closing output file.");
-            }
-        }
+        } 
     }
 }
