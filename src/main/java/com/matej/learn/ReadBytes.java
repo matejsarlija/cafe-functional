9c6cd685883334037e18ad1d17d90ef846c33913
matejsa <matejsarlija1@gmail.com>
2022-03-31 14:52:53 +0200

    and some more classess...

---
+++ src/main/java/com/matej/learn/ReadBytes.java
@@ -0,0 +1,23 @@
+package com.matej.learn;
+
+import java.io.IOException;
+
+public class ReadBytes {
+    public static void main(String[] args) throws IOException {
+        byte[] data = new byte[10];
+
+        /* System.out.println("Enter some characters or digits: ");
+        System.in.read(data);
+
+        System.out.println("You entered: ");
+        for (int i = 0; i < data.length; i++) {
+            System.out.println((char) data[i]);
+        }; */
+
+        int b;
+
+        b = 'X';
+
+        System.out.write(b);
+    }
+}
